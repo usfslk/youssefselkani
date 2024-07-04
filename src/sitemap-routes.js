406a9756f037c4447aab74a233f9da5dca7b1055
Youssef Selkani <ynakles@gmail.com>
2024-07-04 18:00:58 +0100

    sitemap ok

---
+++ src/sitemap-routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+
+    <Route>
+        <Route path="/" />
+        <Route path="/about" />
+        <Route path="/contact" />
+        <Route path="/portfolio" />
+        <Route path="/blog" />
+    </Route>
+
+);
